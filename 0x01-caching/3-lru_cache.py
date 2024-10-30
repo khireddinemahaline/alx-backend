1b02452b1334d459a52948d8f4ac22b7ae3e2aeb
khireddinemahaline <khirozr@gmail.com>
2024-10-30 05:57:29 -0400

    cach system

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -4,3 +4,35 @@
 
 
 BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ LRU caching system
+    """
+    def __init__(self):
+        """ Init
+        """
+        super().__init__()
+        self.keys = []
+
+    def put(self, key, item):
+        """ Add item in the cash in order LRU
+        """
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                key_to_del = self.keys.pop(0)
+                print("DISCARD: {}".format(key_to_del))
+                del self.cache_data[key_to_del]
+            self.cache_data[key] = item
+            if key in self.keys:
+                self.keys.remove(key)
+            self.keys.append(key)
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key in self.cache_data and key:
+            self.keys.remove(key)
+            self.keys.append(key)
+            return self.cache_data[key]
+        return None
